fb237c04aa6cde28004537c81aea1c3a860030f2
Jasper Stam <jasper@codeyellow.nl>
2018-05-28 19:10:15 +0200

    Add initial form logic using Formik

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,22 +1,74 @@
 import React, { Component } from 'react';
+import { Formik } from 'formik';
 
 export default class Form extends Component {
-    handleSubmit() {
-        console.log('TODO submit');
-    }
+    handleSubmit = (
+        values,
+        { setSubmitting, setErrors /* setValues and other goodies */ }
+    ) => {
+        console.log('submit', values);
+    };
+    validate = values => {
+        let errors = {};
+        if (!values.name) {
+            errors.name = 'Required';
+        }
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+        ) {
+            errors.email = 'Invalid email address';
+        }
+        return errors;
+    };
     render() {
         return (
-            <form>
-                <h1>Fill in the form</h1>
-                <label>
-                    Your name
-                    <input />
-                </label>
-                <label>
-                    Your email address
-                    <input />
-                </label>
-            </form>
+            <Formik
+                initialValues={{
+                    name: '',
+                    email: '',
+                }}
+                validate={this.validate}
+                onSubmit={this.handleSubmit}
+                render={({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            Your name
+                            <input
+                                name="name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.name}
+                            />
+                        </label>
+                        {touched.name &&
+                            errors.name && <div>{errors.name}</div>}
+                        <label>
+                            Your email
+                            <input
+                                name="email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email}
+                            />
+                        </label>
+                        {touched.email &&
+                            errors.email && <div>{errors.email}</div>}
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </form>
+                )}
+            />
         );
     }
 }
